96b09671667097d44e71d306ced823dbf59b9188
rohidtzz <63803545+rohidtzz@users.noreply.github.com>
2022-12-22 00:50:47 +0700

    commit docs

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -12,6 +12,34 @@ class BooksController extends Controller
 {
     public function free(){
 
+        // $apiKey= '';
+
+        // $data =[
+        //     'username' => 'asdasdasd',
+        //     'password' => 'asdasd'
+        // ];
+
+        // $curl = curl_init();
+
+        // curl_setopt_array($curl, [
+        //     CURLOPT_FRESH_CONNECT  => true,
+        //     CURLOPT_URL            => 'https://publicapi.hidtzz.my.id/api/register',
+        //     CURLOPT_RETURNTRANSFER => true,
+        //     CURLOPT_HEADER         => false,
+        //     CURLOPT_HTTPHEADER     => ['Authorization:'.$apiKey],
+        //     CURLOPT_FAILONERROR    => false,
+        //     CURLOPT_POST           => true,
+        //     CURLOPT_POSTFIELDS     => http_build_query($data),
+        //     CURLOPT_IPRESOLVE      => CURL_IPRESOLVE_V4
+        // ]);
+
+        // $response = curl_exec($curl);
+        // $error = curl_error($curl);
+
+        // curl_close($curl);
+
+        // echo empty($error) ? $response : $error;
+
         $books = Books::all()->take(10);
 
         $data = [];
